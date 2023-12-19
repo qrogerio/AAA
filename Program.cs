10b0f23b51b53a9fa106d339833e9e869dc3c475
Aluno <aluno@edu.senac.br>
2023-12-19 20:00:50 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine("Sucessor: {0}", numero+1);
+Console.WriteLine("Antercessor: {0}", numero-1);
+
+
